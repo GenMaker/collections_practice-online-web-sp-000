8b136a5e2e546ad809ea6e198726e7c42a67237c
Panela Leung <leungpanela@gmail.com>
2019-08-27 02:21:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,7 +40,7 @@ end
 def add_s(array)
   array.each_with_index.collect do |element, index|
     if index != 1 #THE ELEMENT IS NOT THE SECOND
-    	element[index] << "s"
+    	element[index] + "s"
 	 else
       	#what do we do here?
         return array
